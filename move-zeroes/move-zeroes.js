2fe46710293ac35c46e927149c665dc846097b4b
hatbann <86299528+hatbann@users.noreply.github.com>
2022-03-31 21:24:17 +0900

    Time: 97 ms, Memory: 46.3 MB - LeetHub

--- move-zeroes/move-zeroes.js
+++ move-zeroes/move-zeroes.js
@@ -4,19 +4,17 @@
  */
 var moveZeroes = function(nums) {
     const len = nums.length;
-    let answer = [];
-    let idxlen = 0;
-    for(let i = 0; i<len; i++){
-        if(nums[i] !==0){
-            answer.push(nums[i]);
-            idxlen++;
+    let iterator = 0;
+    let nonzero = 0;
+    for(let nonzero = 0; nonzero<len; nonzero++){
+        if(nums[nonzero] !== 0){
+            if(nums[iterator] === 0){
+                nums[iterator] = nums[nonzero];
+                nums[nonzero] = 0;
+            }
+            iterator++;
         }
     }
-    for(let i = 0; i<len - idxlen; i++){
-        answer.push(0);
-    }
-    for(let i = 0; i<len; i++){
-        nums[i] = answer[i];
-    }
+    
     
 };
