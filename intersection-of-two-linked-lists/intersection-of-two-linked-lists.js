db513381b8be56e2b154b2a942f75297f1578d14
hatbann <86299528+hatbann@users.noreply.github.com>
2022-06-25 15:34:56 +0900

    Time: 156 ms, Memory: 50.3 MB - LeetHub

--- intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
+++ intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
@@ -11,6 +11,8 @@
  * @param {ListNode} headB
  * @return {ListNode}
  */
+
+/*
 var getIntersectionNode = function(headA, headB) {
     let a = headA, b = headB
     while (a !== b) {
@@ -18,4 +20,42 @@ var getIntersectionNode = function(headA, headB) {
         b = !b ? headA : b.next
     }
     return a;
+};
+
+*/
+var getIntersectionNode = function(headA, headB){
+  let indexA = headA, indexB = headB;
+  let lenA = 0, lenB = 0;
+    
+  while(indexA){
+      lenA ++;
+      indexA = indexA.next;
+  }
+    while(indexB){
+        lenB ++;
+        indexB = indexB.next;
+    }
+    if(indexA !== indexB) return null;
+    
+    indexB = headB;
+    indexA = headA;
+    if(lenA < lenB){
+        let lenDiff = lenB-lenA;
+        for(let i = 0; i<lenDiff; i++){
+            indexB = indexB.next;
+        }
+    }
+    else{
+        let lenDiff = lenA-lenB;
+        for(let i = 0; i<lenDiff; i++){
+            indexA = indexA.next;
+        }
+    }
+    
+    while(indexA!==indexB){
+        indexA = indexA.next;
+        indexB = indexB.next;
+    }
+    return indexA;
+    
 };
