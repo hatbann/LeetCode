9efd4ea42ab2475641b45b24b7e462975f00077f
hatbann <86299528+hatbann@users.noreply.github.com>
2022-06-26 16:26:40 +0900

    Time: 107 ms, Memory: 44.7 MB - LeetHub

--- reverse-linked-list/reverse-linked-list.js
+++ reverse-linked-list/reverse-linked-list.js
@@ -10,13 +10,17 @@
  * @return {ListNode}
  */
 var reverseList = function(head) {
-    let prev = null;
-    let cur = head;
-    while(cur){
-        let next = cur.next;  //next는 현재의 다음값으로 설정
-        cur.next = prev; //현재의 다음을 prev로
-        prev = cur;  //과거는 현재가 
-        cur = next; //현재는 미래가 된다.
+    let originHead = head;
+    let currentHead = head;
+    
+    if(originHead === null) return head;
+    
+    while(originHead.next !== null){
+        let index = originHead.next;
+        originHead.next = index.next;
+        index.next = currentHead;
+        currentHead = index;
     }
-    return prev;
+    head = currentHead;
+    return head;
 };
