14be23cb1852e3ac940ef5957938dea4eecf16ad
hatbann <86299528+hatbann@users.noreply.github.com>
2022-07-16 14:21:55 +0900

    Time: 72 ms, Memory: 42.3 MB - LeetHub

--- binary-tree-preorder-traversal/binary-tree-preorder-traversal.js
+++ binary-tree-preorder-traversal/binary-tree-preorder-traversal.js
@@ -11,18 +11,16 @@
  * @return {number[]}
  */
 
-function Traversal(root, list){
-    if(!root) return;
-    list.push(root.val);
-    Traversal(root.left, list);
-    Traversal(root.right, list);
-}
-
 
 var preorderTraversal = function(root) {
-    let preTraversal = new Array();
-    let index = root;
-    Traversal(index,preTraversal);
-    return preTraversal;
+    let answer = [];
     
+    function Traversal(tree){
+        if(!tree) return;
+        answer.push(tree.val);
+        if(tree.left) Traversal(tree.left);
+        if(tree.right) Traversal(tree.right);
+    }
+    Traversal(root);
+    return answer;
 };
