930084399b3c28c598f234fe014cec457651c4cd
hatbann <86299528+hatbann@users.noreply.github.com>
2022-07-16 16:49:14 +0900

    Time: 184 ms, Memory: 101.7 MB - LeetHub

--- construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.js
+++ construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.js
@@ -11,22 +11,16 @@
  * @param {number[]} postorder
  * @return {TreeNode}
  */
-
-
 var buildTree = function(inorder, postorder) {
-    
-    function DFS(arr){
-        if(!arr.length) return null;
-        let rootVal = postorder.pop();
-        let idx = arr.indexOf(rootVal);
-        let newNode = new TreeNode(rootVal);
-        newNode.right = DFS(arr.slice(idx + 1));
-        newNode.left = DFS(arr.slice(0, idx));
-        
-        return newNode;
-        
-    }
-    
-    return DFS(inorder);
 
+    function Traverse(arr){
+        if(!arr.length) return null;
+        let root = postorder.pop();
+        let idx = arr.indexOf(root);
+        let newnode = new TreeNode(root);
+        newnode.right = Traverse(arr.slice(idx+1));
+        newnode.left = Traverse(arr.slice(0, idx));
+        return newnode;
+    }
+    return Traverse(inorder)
 };
