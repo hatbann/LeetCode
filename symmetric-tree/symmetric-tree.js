7650f829441a6b4190843b58880b7cce64b960d8
hatbann <86299528+hatbann@users.noreply.github.com>
2022-07-16 15:16:10 +0900

    Time: 134 ms, Memory: 45 MB - LeetHub

--- symmetric-tree/symmetric-tree.js
+++ symmetric-tree/symmetric-tree.js
@@ -10,39 +10,20 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-
-var isSymmetric = function(root) {
-    if (!root) { // Sanity check
-        return true;
-    }
-
-    // Check if tree s & t are mirroring each other
-    function isMirror(s, t) {
-        if (!s && !t) {
-            return true; // Both nodes are null, ok
-        }
-        if (!s || !t || s.val !== t.val) {
-            return false; // Found a mismatch
-        }
-        // Compare the left subtree of `s` with the right subtree of `t`
-        // and the right subtree of `s` with the left subtree of `t`
-        return isMirror(s.left, t.right) && isMirror(s.right, t.left);
-    }
-
-    return isMirror(root.left, root.right);
-};
-
-
 function mirror(left, right){
     if(!left && !right) return true;
     
     if(!left || !right || left.val !== right.val) return false;
     
-    return mirror(left.left, right.right) && mirror(left.right, right.left);
+    if(!mirror(left.left, right.right)) return false;
+    if(!mirror(left.right, right.left)) return false;
+    
+    return true;
 }
 
+
 var isSymmetric = function(root) {
-    if(!root) return true;
+    if(!root) return false;
     
     return mirror(root.left, root.right);
 };
